caa073b87abbe480fe49d9d149e7c4cc38d9a0bc
cassylee-7 <cassandrasyllee@gmail.com>
2025-04-09 08:44:00 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,21 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int total = 0;
+    for (int i = 0; i < myList.size(); i++) {
+      if (myList.get(i).length() == len)
+        total++;
+    }
+    return total;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    for (int i = 0; i < myList.size(); i++) {
+      if (myList.get(i).length() == len) {
+        myList.remove(i);
+        i--;
+      }
+    }
   }
 }
